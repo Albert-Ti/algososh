cf2599d31d649073869fbd0d8584d804377749ec
Albert Taygibov <albert.ti.2017@yandex.ru>
2023-11-07 03:02:39 +0300

    feat: create algorithm 'string'

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -4,50 +4,73 @@ import { Input } from '../ui/input/input'
 import { Button } from '../ui/button/button'
 import styles from './string.module.css'
 import { Circle } from '../ui/circle/circle'
-
-const swapPairs = (arr: string[], start = 0, end = arr.length - 1) => {
-  if (start >= end) {
-    return arr
-  }
-  ;[arr[start], arr[end]] = [arr[end], arr[start]]
-
-  swapPairs(arr, (start += 1), (end -= 1))
-  return arr
-}
+import { ElementStates } from '../../types/element-states'
 
 export const StringComponent: React.FC = () => {
   const [inputValue, setInputValue] = React.useState('')
-  const [textArray, setTextArray] = React.useState<string[] | null>(null)
+  const [createArray, setCreateArray] = React.useState<string[]>([])
+  const [reverseArray, setReverseArray] = React.useState(createArray)
+  const [done, setDone] = React.useState(true)
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value)
   }
 
-  const handleClick = () => {
-    setTextArray(inputValue.split(''))
+  const handleClickButton = () => {
+    setCreateArray(inputValue.split(''))
+    setReverseArray([])
+    setDone(false)
+  }
 
-    if (textArray) {
-      const newTextArray = [...textArray]
+  React.useEffect(() => {
+    setTimeout(() => {
+      const newTextArray = [...createArray]
+      swapElements(newTextArray)
+    }, 1000)
+  }, [createArray])
 
-      console.log(swapPairs(newTextArray))
-
-      setTimeout(() => {})
+  const swapElements = (arr: string[], start = 0, end = arr.length - 1) => {
+    if (start >= end) {
+      setDone(true)
+      return arr
     }
+    ;[arr[start], arr[end]] = [arr[end], arr[start]]
+
+    setReverseArray([...arr])
+    setTimeout(() => swapElements(arr, start + 1, end - 1), 1000)
   }
 
   return (
     <SolutionLayout title='Строка'>
       <div className={styles.content}>
         <div className={styles.wrapper}>
-          <Input onChange={handleChange} value={inputValue} />
-          <Button onClick={handleClick} text='Развернуть' />
+          <Input onChange={handleChangeInput} value={inputValue} />
+          <Button
+            disabled={inputValue.length > 11}
+            isLoader={!done}
+            onClick={handleClickButton}
+            text='Развернуть'
+          />
         </div>
         <p className={styles.info}>Максимум — 11 символов</p>
       </div>
       <div className={styles.circles}>
-        {textArray?.map((text, i) => (
-          <Circle letter={text} key={i} />
-        ))}
+        {reverseArray.length
+          ? reverseArray.map((text, i) => (
+              <Circle
+                state={
+                  reverseArray[i] !== createArray[i] || done
+                    ? ElementStates.Modified
+                    : reverseArray[i - 1] === createArray[i - 1] &&
+                      reverseArray[i + 1] === createArray[i + 1]
+                    ? ElementStates.Default
+                    : ElementStates.Changing
+                }
+                letter={text}
+                key={i}
+              />
+            ))
+          : createArray.map((text, i) => <Circle letter={text} key={i} />)}
       </div>
     </SolutionLayout>
   )
